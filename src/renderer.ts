bae95041a3fd9376ae7b1fc7042016f230ad8730
Tomas Klingen <tomas.klingen@gmail.com>
2018-12-26 22:34:27 +0100

    update renderer, add thick lines for grid area separation

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,7 @@
 import { div, span, VNode } from "@cycle/dom";
 import { style } from "typestyle";
 import { ValidatedCell } from "./datatypes";
+import { NestedCSSProperties } from "typestyle/lib/types";
 
 export const renderSudoku = (fieldData: ValidatedCell[]): VNode => {
   const cellNodes = fieldData.map(renderGridCell);
@@ -9,28 +10,42 @@ export const renderSudoku = (fieldData: ValidatedCell[]): VNode => {
 };
 
 const renderGridCell = (cell: ValidatedCell): VNode => {
-  let cssClass = `.${cellClass}`;
+  let cssClass = "";
+
   if (cell.value && !cell.isValid) {
-    cssClass += ` .${redClass}`;
+    cssClass = `.${redClass}`;
   }
 
-  return div(cssClass, [span(cell.value)]);
+  return div(cssClass, cell.value || "");
 };
 
 /* CSS */
 
-const cellClass = style({
-  border: "1px solid",
+const cellStyleProps: NestedCSSProperties = {
+  border: "1px solid #000",
   textAlign: "center",
   lineHeight: "70px",
-  fontSize: "2em"
-});
+  fontSize: "2em",
+  $nest: {
+    "&:nth-child(9n - 3), &:nth-child(9n - 6)": {
+      borderRight: "4px solid #000"
+    },
+    "&:nth-child(n + 19):nth-child(-n + 27), &:nth-child(n + 46):nth-child(-n + 54)": {
+      borderBottom: "4px solid #000"
+    }
+  }
+};
 
-const sudokuClass = style({
+const sudokuClass: string = style({
   margin: 50,
   display: "grid",
-  gridTemplate: "repeat(9, 70px) / repeat(9, 70px)",
-  gridGap: 3
+  gridTemplateRows: "repeat(9, 70px)",
+  gridTemplateColumns: "repeat(9, 70px)",
+  $nest: {
+    div: cellStyleProps
+  }
 });
 
-const redClass = style({ color: "red" });
+const redClass = style({
+  color: "red"
+});
