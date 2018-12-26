aa83a7dd54bc778e52c0b19af529e07d15b1edff
Tomas Klingen <tomas.klingen@gmail.com>
2018-12-26 19:08:41 +0100

    WIP: add "area" validation

--- src/validation.ts
+++ src/validation.ts
@@ -52,7 +52,7 @@ export class SudokuValidator {
     if (this.valueExistsInSameCol(value, pos.col)) {
       error |= SudokuError.SameCol;
     }
-    if (this.valueExistsInSameRegion(value, pos)) {
+    if (this.valueExistsInSameRegion(value, index)) {
       error |= SudokuError.SameRegion;
     }
 
@@ -66,8 +66,16 @@ export class SudokuValidator {
     }
   };
 
-  private valueExistsInSameRegion = (value: number, pos: Position) => {
-    return false;
+  private valueExistsInSameRegion = (value: number, index: number) => {
+    const islands = originalSudokuField.filter((island: number[]) =>
+      island.includes(index)
+    );
+
+    const valuesForIsland = islands[0].map(
+      indexInIsland => this.field[indexInIsland]
+    );
+
+    return valuesForIsland.includes(value);
   };
 
   private valueExistsInSameRow = (value: number, row: number) => {
@@ -82,3 +90,23 @@ export class SudokuValidator {
       .includes(value);
   };
 }
+
+const originalSudokuField = [
+  [0, 1, 2, 9, 10, 11, 18, 19, 20],
+  [3, 4, 5, 12, 13, 14, 21, 22, 23],
+  [6, 7, 8, 15, 16, 17, 24, 25, 26],
+  [27, 28, 29, 36, 37, 38, 45, 46, 47],
+  [30, 31, 32, 39, 40, 41, 48, 49, 50],
+  [33, 34, 35, 42, 43, 44, 51, 52, 53],
+  [54, 55, 56, 63, 64, 65, 72, 73, 74],
+  [57, 58, 59, 66, 67, 68, 75, 76, 77],
+  [60, 61, 62, 69, 70, 71, 78, 79, 80]
+];
+
+const hyperSudokuField = [
+  ...originalSudokuField,
+  [10, 11, 12, 19, 20, 21, 28, 29, 30],
+  [14, 15, 16, 23, 24, 25, 32, 33, 34],
+  [46, 47, 48, 55, 56, 57, 64, 65, 66],
+  [50, 51, 52, 59, 60, 61, 68, 69, 70]
+];
